6ce339d511ceaefea6bee01768597ded698cbe9f
m-hou <michaeldamush@gmail.com>
2017-05-25 21:47:28 -0700

    add files

---
+++ gameparser.py
@@ -0,0 +1,12 @@
+import pgn
+import sqlite3
+
+FILE_NAME = "ficsgamesdb_201701_chess_nomovetimes_1465381.pgn"
+
+for game in pgn.GameIterator(FILE_NAME):
+    conn = sqlite3.connect("games.sqlite")
+    c = conn.cursor()
+    g = (game.result, game.blackelo, game.whiteelo, game.timecontrol, ", ".join(game.moves[:-2]))
+    c.execute("INSERT INTO Games VALUES (?,?,?,?,?)", g)
+    conn.commit()
+conn.close()
