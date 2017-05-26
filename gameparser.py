aa70c160430d9a9c3a54d936cf70acad02fe1c47
m-hou <michaeldamush@gmail.com>
2017-05-25 22:57:32 -0700

    whiched to large file and moved commit out of loop

--- gameparser.py
+++ gameparser.py
@@ -1,12 +1,13 @@
 import pgn
 import sqlite3
 
-FILE_NAME = "ficsgamesdb_201701_chess_nomovetimes_1465381.pgn"
+FILE_NAME = "ficsgamesdb_201701_standard2000_nomovetimes_1465638.pgn"
+LARGE_FILE_NAME = "ficsgamesdb_201701_chess_nomovetimes_1465381.pgn"
 
-for game in pgn.GameIterator(FILE_NAME):
+for game in pgn.GameIterator(LARGE_FILE_NAME):
     conn = sqlite3.connect("games.sqlite")
     c = conn.cursor()
     g = (game.result, game.blackelo, game.whiteelo, game.timecontrol, ", ".join(game.moves[:-2]))
     c.execute("INSERT INTO Games VALUES (?,?,?,?,?)", g)
-    conn.commit()
+conn.commit()
 conn.close()
