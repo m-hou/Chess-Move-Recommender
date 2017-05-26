6d3d70fdc7457ea81ca4eb8dde96ea034a9984bd
m-hou <michaeldamush@gmail.com>
2017-05-25 23:29:55 -0700

    moved connecter out of loop

--- gameparser.py
+++ gameparser.py
@@ -4,10 +4,14 @@ import sqlite3
 FILE_NAME = "ficsgamesdb_201701_standard2000_nomovetimes_1465638.pgn"
 LARGE_FILE_NAME = "ficsgamesdb_201701_chess_nomovetimes_1465381.pgn"
 
-for game in pgn.GameIterator(LARGE_FILE_NAME):
+def addGamesToDB():
     conn = sqlite3.connect("games.sqlite")
     c = conn.cursor()
-    g = (game.result, game.blackelo, game.whiteelo, game.timecontrol, ", ".join(game.moves[:-2]))
-    c.execute("INSERT INTO Games VALUES (?,?,?,?,?)", g)
-conn.commit()
-conn.close()
+    for index, game in enumerate(pgn.GameIterator(LARGE_FILE_NAME)):
+        g = (game.result, game.blackelo, game.whiteelo, game.timecontrol, ", ".join(game.moves[:-2]))
+        c.execute("INSERT INTO Games VALUES (?,?,?,?,?)", g)
+        print(index)
+    conn.commit()
+    conn.close()
+
+addGamesToDB()
