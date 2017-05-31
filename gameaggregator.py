c65dded394a32a1fc9a92ad2f504853b085b7a0d
m-hou <michaeldamush@gmail.com>
2017-05-30 20:54:49 -0700

    show popular next moves, given current moves

--- gameaggregator.py
+++ gameaggregator.py
@@ -4,10 +4,16 @@ def queryDB(moves):
     """doc"""
     conn = sqlite3.connect("games.sqlite")
     c = conn.cursor()
-    c.execute("SELECT count(*) FROM Games WHERE Moves LIKE ?", (moves,))
-    count = c.fetchone()
+    c.execute("""SELECT SUBSTR(Moves, ?, INSTR(SUBSTR(Moves, ?), ?)) AS nextmove, count(*)
+                 FROM Games
+                 WHERE Moves
+                 LIKE ?
+                 GROUP BY nextmove
+                 ORDER BY count(*) DESC
+                 LIMIT 20""", (len(moves) + 3, len(moves) + 4, ",", moves + "%"))
+    count = c.fetchall()
     conn.close()
     return count
 
 moves = input('Enter moves: ')
-print(*queryDB(moves + "%"))
+print(*queryDB(moves))
