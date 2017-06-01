09576ccfdf55e5da7bac8c387d47df320a060c4c
m-hou <michaeldamush@gmail.com>
2017-05-31 22:44:51 -0700

    add function to group moves by winrate

--- gameaggregator.py
+++ gameaggregator.py
@@ -1,5 +1,29 @@
 import sqlite3
 
+def winningMoves(moves):
+    """doc"""
+    conn = sqlite3.connect("games.sqlite")
+    c = conn.cursor()
+    c.execute("""SELECT SUBSTR(Moves, ?, INSTR(SUBSTR(Moves, ?), ?)) AS nextmove,
+                 ROUND(
+                    SUM(
+                        CASE WHEN Result = '1-0' THEN 1
+                        WHEN Result = '1/2-1/2' THEN 0.5
+                        WHEN Result = '0-1' THEN 0
+                        ELSE 0
+                        END
+                    ) / count(*),
+                 4) as winrate
+                 FROM Games
+                 WHERE Moves
+                 LIKE ?
+                 GROUP BY nextmove
+                 ORDER BY winrate DESC
+                 LIMIT 20""", (len(moves) + 3, len(moves) + 4, ",", moves + "%"))
+    count = c.fetchall()
+    conn.close()
+    return count
+
 def popularMoves(moves):
     """doc"""
     conn = sqlite3.connect("games.sqlite")
@@ -17,3 +41,4 @@ def popularMoves(moves):
 
 moves = input('Enter moves: ')
 print(*popularMoves(moves))
+print(*winningMoves(moves))
