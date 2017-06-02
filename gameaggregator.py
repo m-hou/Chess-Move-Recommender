28a3a46ba3cc7c05440510ad41064fccb268a3d2
m-hou <michaeldamush@gmail.com>
2017-06-01 17:53:15 -0700

    added decorator decorator and updated timed call output to print some values instead of return

--- gameaggregator.py
+++ gameaggregator.py
@@ -4,14 +4,24 @@ from functools import update_wrapper
 
 DB = "games.sqlite"
 
+def decorator(d):
+    """doc"""
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+decorator = decorator(decorator)
+
+@decorator
 def timedcall(f):
     def timedcall_f(*arg, **kw):
         """doc"""
         start = time.time()
         res = f(*arg, **kw)
         end = time.time()
-        return (end - start), res, f.__name__
-    update_wrapper(timedcall_f, f)
+        print("%s took %fs to execute." % (f.__name__, end - start))
+        return res
     return timedcall_f
 
 def queryDB(query, *args):
